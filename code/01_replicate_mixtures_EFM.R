418a9a4301837374ba63956b2b0207f9f212aa8e
Kai <64656013+kaibukaibu@users.noreply.github.com>
2026-02-16 17:38:09 +0100

    Simulate with EFM

--- code/01_replicate_mixtures_EFM.R
+++ code/01_replicate_mixtures_EFM.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(euroformix)
+library(rlist)
+source("code/helping_functions.R")
 set.seed(66)
 
 #setwd("..")
@@ -8,14 +10,12 @@ set.seed(66)
 input_parameters_fileloc <- "LR_results/replicates_unfiltered001riman.csv"
 output <- "data/data_replicated/"
 pop <- "data/NIST1036_Cauc.csv"
+input_1p_fileloc <- "data/data_provedit_cleaned/genotypes.csv"
 
 
 
-# # make results subfolders
-# if (dir.exists(output)) {
-#   unlink(output, recursive = TRUE, force = T)  # Deletes the folder and all contents
-#   dir.create(output)
-# }
+
+# make results subfolders
 for(NOC in 2:4){
   for(type in c("EFM")){
     dir.create(paste0(output, NOC, "p_", type),
@@ -33,17 +33,31 @@ input_parameters <- read.csv2(input_parameters_fileloc) %>%  #should be of size 
   filter(Hu_expectedPeakHeight == sample(Hu_expectedPeakHeight,1))
 
 
+
+# mixture contributors
+input_1p <- read.csv(input_1p_fileloc) %>%
+  pivot_longer(cols=c("Allele.1", "Allele.2")) %>%
+  rename(Allele = value) %>%
+  select(!name) %>%
+  mutate(Sample.Name = gsub("c", "", Sample.Name),
+         Allele = as.character(as.numeric(Allele)))
+
+
+
 # dataset of popfreqs
 popfreqs <- read.csv(pop) %>% 
   pivot_longer(cols=2:25) %>%
   rename(freq = value,
          Marker = name) %>%
-  filter(!is.na(freq))
+  filter(!is.na(freq)) %>% 
+  filter(Marker %in% unique(input_1p$Marker))
 
 popfreqs2 <- lapply(split(popfreqs, popfreqs$Marker), function(x) {
   freqs <- x$freq
   names(freqs) <- x$Allele
   freqs })
+
+
 
 
 for(i in 1:nrow(input_parameters)){
@@ -74,34 +88,76 @@ for(i in 1:nrow(input_parameters)){
   props00 <- as.numeric(na.exclude(c(prop1, prop2, prop3, prop4)))
   props <- props00 / sum(props00)
   
+  
+  # Get contributors from naming convention
+  contr1 <- str_split_i(tracerow$trace, "-", 4)
+  contr2 <- str_split_i(tracerow$trace, "-", 5)
+  contr3 <- case_when(
+    tracerow$NoC==2 ~ NA,
+    tracerow$NoC %in% c(3,4) ~ str_split_i(tracerow$trace, "-", 6)
+  )
+  contr4 <- case_when(
+    tracerow$NoC %in% c(2,3) ~ NA,
+    tracerow$NoC==4 ~ str_split_i(tracerow$trace, "-", 7)
+  )
+  
+  contrs <- as.character(na.exclude(c(contr1, contr2, contr3, contr4)))
+  
+  # Loop over contrs, read in their genotypes and save them in a format necessary for EFM
+  locs <- unique(input_1p$Marker)
+  refdat <- setNames(vector("list", length(locs)), locs)
+  
+  for (j in seq_along(contrs)) {
+    cont <- contrs[j]
+    sub <- input_1p %>% filter(Sample.Name == cont)
+    
+    byloc <- split(sub, sub$Marker)
+    
+    for (loc in names(byloc)) {
+      alleles <- byloc[[loc]]$Allele
+      refdat[[loc]][[j]] <- alleles
+      names(refdat[[loc]])[j] <- paste0("genref", j)
+    }
+  }
+  
+  
+  
+  
+  # Sample one mixture per PROVEDIt mixture
   sampled <- genDataset(
     nC = tracerow$NoC,
     popFreq = popfreqs2,
     mu = tracerow$Hu_expectedPeakHeight,
     sigma = tracerow$Hu_peakHeightVariance,
     threshT = 0,
-    #refData = #somehow add contributors,
+    refData = refdat,
     mx = props,
     nrep = 1,
     stutt = 0,
     stuttFW = 0,
     prC = 0.05, #default of DNAStatistX
     lambda = 0.01,
-    beta = tracerow$Hu_degradationSlope,
-    kit = "GlobalFiler"
+    beta = tracerow$Hu_degradationSlope#, 
+    #kit = "GlobalFiler"
   )
-  # plotEPG2(sampled$samples,
-  #          kit = "GlobalFiler",
-  #          refData=sampled$refData,
-  #          AT=50)
+  
+  #plotEPG2(sampled$samples,kit = "GlobalFiler",AT=0) #visualize samples
+  
+  mixture <- map_df(names(sampled$samples), function(samp) {
+    map_df(names(sampled$samples[[samp]]), function(loc) {
+      dat <- sampled$samples[[samp]][[loc]]
+      tibble(
+        Marker = loc,
+        Allele = dat$adata,
+        Height = dat$hdata
+      )
+    })
+  }) %>% 
+    long_to_wide(samplelabel = paste0("EFM-", gsub(".csv", "", tracerow$trace)))
+  
+  write.csv(mixture, paste0(output, tracerow$NoC, "p_", type, "/", unique(mixture$SampleName), ".csv"),
+            row.names=F, quote = F)
 }
-
-
-
-
-
-
-
 
 
 
